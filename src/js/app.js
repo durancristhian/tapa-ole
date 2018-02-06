0379bede66c28de9df3288270304b3d6ec40030e
cristhian <durancristhian@gmail.com>
2018-02-06 19:23:30 -0300

    add previsualization step

--- src/js/app.js
+++ src/js/app.js
@@ -58,6 +58,7 @@ function handleFileSelect(event) {
                         </div>
                     </div>
                 `;
+                userCoverEl.classList.remove('hide');
 
                 postToImgur();
             };
