d2829f17fffa045d4b723e8b1f666c5b8d83b8d6
Cristhian Duran <durancristhian@gmail.com>
2020-03-16 18:50:47 -0300

    Mejoras

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,26 +4,28 @@ const { version } = require('../../package.json')
 
 export default function Header() {
   return (
-    <div className="border-b-2 border-gray-300 bg-gray-200">
-      <div className="flex items-center justify-between max-w-3xl mx-auto px-4 py-2">
-        <div className="flex items-center">
-          <h1 className="font-bold text-2xl uppercase">tapa-ole</h1>
-          <span className="ml-4 text-sm">v{version}</span>
+    <div className="bg-white shadow">
+      <div className="max-w-3xl mx-auto px-4 py-2">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <h1 className="font-bold text-2xl uppercase">tapa-ole</h1>
+            <span className="ml-4 text-sm">v{version}</span>
+          </div>
+          <p className="flex items-center text-sm">
+            <span>por&nbsp;</span>
+            <a
+              className={classnames([
+                'text-gray-600 underline',
+                'focus:outline-none focus:shadow-outline focus:text-gray-800 hover:text-gray-800',
+              ])}
+              href="https://twitter.com/DuranCristhian"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @durancristhian
+            </a>
+          </p>
         </div>
-        <p className="flex items-center text-sm">
-          <span>por&nbsp;</span>
-          <a
-            className={classnames([
-              'text-gray-600 underline',
-              'focus:outline-none focus:shadow-outline focus:text-gray-800 hover:text-gray-800',
-            ])}
-            href="https://twitter.com/DuranCristhian"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            @durancristhian
-          </a>
-        </p>
       </div>
     </div>
   )
